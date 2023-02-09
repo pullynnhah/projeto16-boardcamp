1c5a0b9aaabeb50eaa51437d25bdfdc7d7b90da1
Paula Rodrigues <pullynnhah@icloud.com>
2023-02-09 16:20:30 -0300

    :recycle: rewrite db connection

--- src/database/db.connection.js
+++ src/database/db.connection.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL
-};
 
-configDatabase.ssl = process.env.MODE === "prod";
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.MODE === "prod"
+});
 
-const db = new Pool(configDatabase);
 export default db;
